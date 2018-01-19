0d02249c326519046f32a65ed53b2131891ce874
Baseln <basel.obeid9661@gmail.com>
2018-01-19 17:36:45 +0100

    Added a wrapper to RadioButton
    Added a wrapper to RadioButton to give the ability to resize touch place

--- index.js
+++ index.js
@@ -7,36 +7,40 @@ import {
     TouchableWithoutFeedback
 } from 'react-native';
 
-export default class RadioButton extends Component{
+export default class RadioButton extends Component {
     
     render(){
-    return(
-            <TouchableWithoutFeedback onPress={() => this.props.onPress(this.props.value)}>
-        <View style={{ flexDirection: 'row' }}>
-            <View style={[{
-            height: this.props.outerCircleSize || 24,
-            width: this.props.outerCircleSize || 24,
-            borderRadius: this.props.outerCircleSize/2 || 12,
-            borderWidth: this.props.outerCircleWidth || 2,
-            borderColor: this.props.outerCircleColor || '#9eacb4',
-            alignItems: 'center',
-            justifyContent: 'center',
-            }]}>
-            {
-            this.props.value===this.props.currentValue ?
-            <View style={{
-              height: this.props.innerCircleSize || 12,
-              width: this.props.innerCircleSize || 12,
-              borderRadius: this.props.innerCircleSize/2 || 6,
-              backgroundColor: this.props.innerCircleColor || '#ff6624',
-            }}/>
-            : null
-            }
+        return(
+            <View style={this.props.wrapperStyle}>
+              <TouchableWithoutFeedback onPress={() => this.props.onPress(this.props.value)}>
+                <View style={{ flexDirection: 'row' }}>
+                    <View style={[{
+                    height: this.props.outerCircleSize || 24,
+                    width: this.props.outerCircleSize || 24,
+                    borderRadius: this.props.outerCircleSize/2 || 12,
+                    borderWidth: this.props.outerCircleWidth || 2,
+                    borderColor: this.props.outerCircleColor || '#9eacb4',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    }]}>
+                    {
+                    this.props.value===this.props.currentValue ?
+                    <View style={{
+                      height: this.props.innerCircleSize || 12,
+                      width: this.props.innerCircleSize || 12,
+                      borderRadius: this.props.innerCircleSize/2 || 6,
+                      backgroundColor: this.props.innerCircleColor || '#ff6624',
+                    }}/>
+                    : null
+                    }
+                    </View>
+                       {this.props.children}
+                    </View>
+               </TouchableWithoutFeedback>
             </View>
-`               {this.props.children}
-            </View>
-            </TouchableWithoutFeedback>
-    );
+        );
+    }
 }
-
+RadioButton.defaultProps = {
+    wrapperStyle: {}
 }
